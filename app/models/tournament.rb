eca1043e40f882cd7ea32b2019102cf4b1e65408
ignored-comment <info.konham@gmail.com>
2021-10-04 19:11:58 -0400

    Validates presence of name for Tournament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,5 @@
 class Tournament < ApplicationRecord
+    validates_presence_of :name
     has_many :teams
     has_many :assessments
 end
--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Tournament, type: :model do
   describe 'validations' do
     it { should have_many(:teams) }
     it { should have_many(:assessments) }
+
+    it { should validate_presence_of(:name) }
   end
 end
