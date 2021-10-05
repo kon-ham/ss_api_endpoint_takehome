b9e9b5c5c9d49f4f882b62add0726c26da42c2f2
ignored-comment <info.konham@gmail.com>
2021-10-04 21:08:51 -0400

    Change user_spec for better readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password) }
-
+  describe 'relationships' do
     it { should have_many(:assessments) }
     it { should have_many(:notes).through(:assessments) }
+  end
+  
+  describe '#email' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
+  
+  describe '#password' do
+    it { should validate_presence_of(:password) }
   end
 end
