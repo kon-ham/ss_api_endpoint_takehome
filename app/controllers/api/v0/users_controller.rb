aaeb7c848e3f7aaa4f4bbfc07f10f597a20a50ef
ignored-comment <info.konham@gmail.com>
2021-10-05 16:48:46 -0400

    Add 1 user to seed file, enable CORS, comment out before_authorized action in Application controller

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,3 +1,22 @@
 class Api::V0::UsersController < ApplicationController
-    
+
+    def login
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+    def auto_login
+        render json: @user
+    end
+
+    private
+
+    def user_params
+        params.permit(:email, :password)
+    end
 end
--- spec/requests/v0/login/user_spec.rb
+++ spec/requests/v0/login/user_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'User Login' do
-    before :each do
-        @user = User.create!(
-            email: 'jingleheimerschmitt@jacob.com',
-            password: 'asdf1234'
-        )
+    before :all do
+        @user = User.create!(email: 'johnnyappleseed@seed.com', password: "lmaolol")
     end
-
-#     describe 'POST /login' do
-#         it 'the /login endpoint exists' do
-#             post '/api/v0/login'
-#             expect(response).to be_successful
-#             expect(response.status).to eq(204)
-#         end
-#     end
+    describe 'POST /login' do
+        it 'the /login endpoint exists' do
+            post '/api/v0/login', params: { email: @user.email, password: @user.password }
+            binding.pry
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+        end
+    end
 end
