dccd43e54ab8c0c373259169b94a109ee9ef32a2
ignored-comment <info.konham@gmail.com>
2021-10-07 22:03:02 -0400

    Pass test assertions for GET /players/{:player_id}

--- app/controllers/api/v0/players_controller.rb
+++ app/controllers/api/v0/players_controller.rb
@@ -5,4 +5,11 @@ class Api::V0::PlayersController < ApplicationController
         players = Player.all
         render json: PlayerSerializer.new(players)
     end
+
+    def show
+        player = Player.find(params[:id])
+        render json: PlayerSerializer.new(player)
+    rescue
+        render json: { message: 'Invalid player request' }, status: 404
+    end
 end
--- spec/requests/v0/player/player_spec.rb
+++ spec/requests/v0/player/player_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper' 
+# Please be advised: currently the model does not reflect a relationship with all users,
+# so the endpoint returns ALL players instead of players associated with the user
 
 RSpec.describe 'Players' do
     before :each do
@@ -14,8 +16,6 @@ RSpec.describe 'Players' do
     end
 
     describe 'Happy Path - GET /players' do
-        # Please be advised: currently the model does not reflect a relationship with all users,
-        # so the endpoint returns ALL players instead of players associated with the user
         it 'can reach the GET /players endpoint and return all players' do
             get '/api/v0/players', headers: @headers
 
@@ -41,11 +41,27 @@ RSpec.describe 'Players' do
     end
 
     describe 'Happy Path - GET /players/{:player_id}' do
-        it 'can reach the GET /playres/{:player_id} and return a specific player' do
-            get "/api/v0/player/#{@player.id}"
+        it 'can reach the GET /players/{:player_id} and return a specific player' do
+            get "/api/v0/players/#{@player.id}", headers: @headers
+
+            json_response = JSON.parse(response.body, symbolize_names: true)
 
             expect(response).to be_successful
             expect(response.status).to eq(200)
+            expect(json_response).to have_key(:data)
+            expect(json_response[:data].count).to eq(3)
+            expect(json_response[:data]).to have_key(:id)
+            expect(json_response[:data]).to have_key(:type)
+            expect(json_response[:data]).to have_key(:attributes)
+            expect(json_response[:data][:attributes].count).to eq(8)
+            expect(json_response[:data][:attributes]).to have_key(:first_name)
+            expect(json_response[:data][:attributes]).to have_key(:last_name)
+            expect(json_response[:data][:attributes]).to have_key(:height)
+            expect(json_response[:data][:attributes]).to have_key(:weight)
+            expect(json_response[:data][:attributes]).to have_key(:birthday)
+            expect(json_response[:data][:attributes]).to have_key(:graduation_year)
+            expect(json_response[:data][:attributes]).to have_key(:position)
+            expect(json_response[:data][:attributes]).to have_key(:recruit)
         end
     end
 end
