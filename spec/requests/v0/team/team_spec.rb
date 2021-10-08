5fd6a45225f5e6388f1122bf11a02ac458aa8c12
ignored-comment <info.konham@gmail.com>
2021-10-08 14:24:23 -0400

    Pass test assertion for both include w/o include for get teams/{:team_id}

--- spec/requests/v0/team/team_spec.rb
+++ spec/requests/v0/team/team_spec.rb
@@ -20,9 +20,81 @@ RSpec.describe 'Teams' do
 
     describe 'Happy Path - GET /teams/{:team_id}' do
         it 'can successfully reach the GET /teams/{:team_id} with include players params' do
-            get "/api/v0/teams/#{@team.id}", headers: @header, params: { include: "players" }
+            get "/api/v0/teams/#{@team.id}", headers: @headers, params: { include: "players" }
+
+            json_response = JSON.parse(response.body, symbolize_names: true )
 
             expect(response).to be_successful
+
+            expect(json_response.keys.count).to eq(2)
+            expect(json_response).to have_key(:data)
+            expect(json_response).to have_key(:included)
+
+            expect(json_response[:data].keys.count).to eq(4)
+            expect(json_response[:data]).to have_key(:id)
+            expect(json_response[:data]).to have_key(:type)
+            expect(json_response[:data]).to have_key(:attributes)
+            expect(json_response[:data]).to have_key(:relationships)
+
+            expect(json_response[:data][:attributes].keys.count).to eq(2)
+            expect(json_response[:data][:attributes]).to have_key(:name)
+            expect(json_response[:data][:attributes]).to have_key(:age_group)
+
+            expect(json_response[:data][:relationships].keys.count).to eq(1)
+            expect(json_response[:data][:relationships]).to have_key(:players)
+            
+            expect(json_response[:data][:relationships][:players].keys.count).to eq(1)
+            expect(json_response[:data][:relationships][:players]).to have_key(:data)
+
+            expect(json_response[:data][:relationships][:players][:data].first.keys.count).to eq(2)
+            expect(json_response[:data][:relationships][:players][:data].first).to have_key(:id)
+            expect(json_response[:data][:relationships][:players][:data].first).to have_key(:type)
+
+            expect(json_response[:included].first.keys.count).to eq(3)
+            expect(json_response[:included].first).to have_key(:id)
+            expect(json_response[:included].first).to have_key(:type)
+            expect(json_response[:included].first).to have_key(:attributes)
+
+            expect(json_response[:included].first[:attributes].keys.count).to eq(8)
+            expect(json_response[:included].first[:attributes]).to have_key(:first_name)
+            expect(json_response[:included].first[:attributes]).to have_key(:last_name)
+            expect(json_response[:included].first[:attributes]).to have_key(:height)
+            expect(json_response[:included].first[:attributes]).to have_key(:weight)
+            expect(json_response[:included].first[:attributes]).to have_key(:graduation_year)
+            expect(json_response[:included].first[:attributes]).to have_key(:position)
+            expect(json_response[:included].first[:attributes]).to have_key(:birthday)
+            expect(json_response[:included].first[:attributes]).to have_key(:recruit)
+        end
+    end
+
+    describe 'Happy Path - GET /teams/{:team_id}' do
+        it 'can successfully reach the GET /teams/{:team_id} with out include players params' do
+            get "/api/v0/teams/#{@team.id}", headers: @headers
+
+            json_response = JSON.parse(response.body, symbolize_names: true )
+
+            expect(response).to be_successful
+
+            expect(json_response.keys.count).to eq(1)
+            expect(json_response).to have_key(:data)
+
+            expect(json_response[:data].keys.count).to eq(4)
+            expect(json_response[:data]).to have_key(:id)
+            expect(json_response[:data]).to have_key(:type)
+            expect(json_response[:data]).to have_key(:attributes)
+            expect(json_response[:data]).to have_key(:relationships)
+
+            expect(json_response[:data][:attributes].keys.count).to eq(2)
+            expect(json_response[:data][:attributes]).to have_key(:name)
+            expect(json_response[:data][:attributes]).to have_key(:age_group)
+
+            expect(json_response[:data][:relationships]).to have_key(:players)
+            expect(json_response[:data][:relationships][:players].keys.count).to eq(1)
+            expect(json_response[:data][:relationships][:players]).to have_key(:data)
+
+            expect(json_response[:data][:relationships][:players][:data].first.keys.count).to eq(2)
+            expect(json_response[:data][:relationships][:players][:data].first).to have_key(:id)
+            expect(json_response[:data][:relationships][:players][:data].first).to have_key(:type)
         end
     end
 end
