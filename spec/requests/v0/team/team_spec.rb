a83c9806eb95ec83b8c2ef105218fa086e9318e4
ignored-comment <info.konham@gmail.com>
2021-10-08 13:22:59 -0400

    Create failing test assertion for GET /teams/{:team_id}

--- spec/requests/v0/team/team_spec.rb
+++ spec/requests/v0/team/team_spec.rb
@@ -20,6 +20,9 @@ RSpec.describe 'Teams' do
 
     describe 'Happy Path - GET /teams/{:team_id}' do
         it 'can successfully reach the GET /teams/{:team_id} with include players params' do
+            get "/teams/#{@team.id}", headers: @header, params: { include: "players" }
+
+            expect(response).to be_successful
         end
     end
 end
